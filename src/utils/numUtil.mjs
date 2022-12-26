c3b0ede2827a4c3860f6d51f774443a07c0f6761
SpiderPig60 <spiderpig60@gmail.com>
2022-12-26 19:41:24 +0200

    added utils

--- src/utils/numUtil.mjs
+++ src/utils/numUtil.mjs
@@ -8,3 +8,11 @@ export const numToDigits = (num = 0) => {
 
     return digits;
 }
+
+// "floor" rounds upward (if 0.5 turns to 1)
+export const numToFixed = (num = 0, digit = 0, { base = 10, roundFunc = 'floor' } = {}) => {
+    const pow = Math.pow(base, digit);
+    return Math[roundFunc](num * pow) / pow;
+};
+
+export const numIsBetween = (num = 0, min = 0, max = 0) => num >= min && num <= max;
