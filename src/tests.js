fd9753b17013030416b089d5b73ab5f283fdafc6
SpiderPig60 <spiderpig60@gmail.com>
2023-02-07 23:45:31 +0200

    refractoring

--- src/tests.js
+++ src/tests.js
@@ -1,79 +1,8 @@
-import { isArr } from "./utils/pure/arrUtil.js";
-import KeyArray from "./utils/classes/KeyArray.js";
-import { hasOwn } from "./utils/pure/objUtil.js";
+import KeyArray from './classes/KeyArray.js'
 
-
-
-const forInDeep = (obj, cb) => {
-    for (let key in obj) {
-        if (!obj.hasOwnProperty(key)) continue;
-        const value = obj[key];
-
-        if (typeof value === 'object') {
-            forInDeep(value, cb);
-        } else {
-            cb(value, key);
-        }
-    }
-}
-
-export const forInDeepBreak = (obj, cb) => {
-    let toStop = false;
-
-    (function forInDeepBreakHelper(obj) {
-        for (let key in obj) {
-            if (toStop === true) return;
-            if (!hasOwn(obj, key)) continue;
-            const value = obj[key];
-
-            if (typeof value === 'object') {
-                forInDeepBreakHelper(value);
-
-            } else {
-                const toBreak = cb(value, key);
-                if (toBreak === true) toStop = true;
-            }
-        }
-    })(obj)
-}
-
-
-const obj = {
-    test: [1, 2, { test2: 'abc' }, 4],
-    b: { 'c': 5 },
-    a: {
-        b: [
-            5,
-            2,
-            {
-                nested: ['b', 'c', 'd', {
-                    mostNested: '83'
-                }]
-            },
-            3
-        ]
-    }
-}
-
-const arr = [{ test3: 'yxz' }, obj]
-
-
-// forInDeepBreak(arr, (value, key) => {
-//     console.log('iteration')
-//     if (value === 'c') {
-//         console.log('STOP')
-//         return true
-//     };
-// })
 
 const karr = new KeyArray({ array: ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'g', 'h', 'i', 'j', 'k'] });
-console.log(`
 
-
-HERE
-
-
-`);
 console.table(karr.toArray())
 
 // karr.insertByKey([1, 2, 3, 4, 5, 6, 7], 'k')
