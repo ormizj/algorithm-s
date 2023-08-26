2cbb91a9c37b7b87df874080947838882029cfb5
Or <spiderpig60@gmail.com>
2023-08-26 21:52:06 +0300

    updated WIP

--- src/leet-code/problems/medium/646-maximum-length-of-pair-chain/a.js
+++ src/leet-code/problems/medium/646-maximum-length-of-pair-chain/a.js
@@ -8,8 +8,10 @@ var findLongestChain = function (pairs) {
     let endingKey = pairs[0][0];
 
     for (const pair of pairs) {
+        // place chain leap in its starting position
         pairsMap[pair[0]] = pair[1] - pair[0];
 
+        // get minimum key, maximum key
         if (startingKey > pair[0]) startingKey = pair[0];
         else if (endingKey < pair[0]) endingKey = pair[0];
     }
@@ -17,19 +19,18 @@ var findLongestChain = function (pairs) {
     const checkPairs = (pairKey, longestChain) => {
         if (pairKey > endingKey) return longestChain;
 
-        let addedChain = longestChain;
-        let newPairKey = pairKey;
-
-        const leap = pairsMap[pairKey] + 1;
-        if (pairsMap[leap] !== undefined) {
-            addedChain++;
-            newPairKey += pairsMap[leap];
+        const pairLeap = pairsMap[pairKey] ?? 0;
+        const chainKey = pairKey + pairLeap + 1;
+        if (pairsMap[chainKey] !== undefined) {
+            // if new chain exists test this chain, continue testing other chains
+            return Math.max(
+                checkPairs(chainKey, longestChain + 1),
+                checkPairs(pairKey + 1, longestChain),
+            );
         }
 
-        return Math.max(
-            checkPairs(pairKey + 1, longestChain),
-            checkPairs(newPairKey, addedChain),
-        );
+        // if no new chain exists, continue
+        return checkPairs(pairKey + 1, longestChain)
     }
 
     return checkPairs(startingKey, 1);
@@ -42,7 +43,7 @@ import { printEnd, printResult } from "../../../answerUtil.js";
 
 const answerCb = findLongestChain;
 printResult({ answerCb, expected: 2, input: { pairs: [[1, 2], [2, 3], [3, 4]] } });
-// printResult({ answerCb, expected: 3, input: { pairs: [[1, 2], [7, 8], [4, 5]] } });
-// printResult({ answerCb, expected: 4, input: { pairs: [[-10, -8], [8, 9], [-5, 0], [6, 10], [-6, -4], [1, 7], [9, 10], [-4, 7]] } });
+printResult({ answerCb, expected: 3, input: { pairs: [[1, 2], [7, 8], [4, 5]] } });
+printResult({ answerCb, expected: 4, input: { pairs: [[-10, -8], [8, 9], [-5, 0], [6, 10], [-6, -4], [1, 7], [9, 10], [-4, 7]] } });
 
 printEnd();
