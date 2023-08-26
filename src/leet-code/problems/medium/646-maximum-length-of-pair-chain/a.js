d7d920d9637b41afb317d0282ea08f5a443c2dfc
Or <spiderpig60@gmail.com>
2023-08-26 23:47:21 +0300

    WIP

--- src/leet-code/problems/medium/646-maximum-length-of-pair-chain/a.js
+++ src/leet-code/problems/medium/646-maximum-length-of-pair-chain/a.js
@@ -8,8 +8,10 @@ var findLongestChain = function (pairs) {
     let endingKey = pairs[0][0];
 
     for (const pair of pairs) {
-        // place chain leap in its starting position
-        pairsMap[pair[0]] = pair[1] - pair[0];
+        // place LOWEST chain leap in its starting position
+        const pairLeap = pair[1] - pair[0];
+        if (pairsMap[pair[0]] === undefined) pairsMap[pair[0]] = pairLeap;
+        else if (pairsMap[pair[0]] > pairLeap) pairsMap[pair[0]] = pairLeap;
 
         // get minimum key, maximum key
         if (startingKey > pair[0]) startingKey = pair[0];
@@ -22,6 +24,8 @@ var findLongestChain = function (pairs) {
         const pairLeap = pairsMap[pairKey] ?? 0;
         const chainKey = pairKey + pairLeap + 1;
         if (pairsMap[chainKey] !== undefined) {
+            // console.log(pairsMap[chainKey]);
+
             // if new chain exists test this chain, continue testing other chains
             return Math.max(
                 checkPairs(chainKey, longestChain + 1),
@@ -45,5 +49,6 @@ const answerCb = findLongestChain;
 printResult({ answerCb, expected: 2, input: { pairs: [[1, 2], [2, 3], [3, 4]] } });
 printResult({ answerCb, expected: 3, input: { pairs: [[1, 2], [7, 8], [4, 5]] } });
 printResult({ answerCb, expected: 4, input: { pairs: [[-10, -8], [8, 9], [-5, 0], [6, 10], [-6, -4], [1, 7], [9, 10], [-4, 7]] } });
+printResult({ answerCb, expected: 3, input: { pairs: [[-6, 9], [1, 6], [8, 10], [-1, 4], [-6, -2], [-9, 8], [-5, 3], [0, 3]] } });
 
 printEnd();
